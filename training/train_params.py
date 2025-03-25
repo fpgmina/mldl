4f4617b3bd18c2f7694ca9ffdf895c62ef143fd6
fpgmina <francesco.mina1986@gmail.com>
2025-03-25 11:01:47 -0400

    up train_params

--- training/train_params.py
+++ training/train_params.py
@@ -3,8 +3,20 @@ import torch
 from torch import nn
 from typing import Optional, Dict, Any
 
+def is_nn_module(instance, attribute, value):
+    """Validator to check if the value is an instance of nn.Module or its subclass."""
+    if not isinstance(value, nn.Module):
+        raise TypeError(f"{attribute.name} must be an instance of nn.Module or its subclass.")
+    return value
+
+def is_optimizer(instance, attribute, value):
+    """Validator to check if the value is an instance of torch.optim.Optimizer."""
+    if not isinstance(value, torch.optim.Optimizer):
+        raise TypeError(f"{attribute.name} must be an instance of torch.optim.Optimizer.")
+    return value
+
 @attr.s(frozen=True, kw_only=True)
-class TrainingParams():
+class TrainingParams:
     """
     A class to store the parameters required for training a model.
     
@@ -20,7 +32,7 @@ class TrainingParams():
     training_name: str = attr.ib(validator=attr.validators.instance_of(str))
     epochs: int = attr.ib(validator=attr.validators.instance_of(int))
     learning_rate: float = attr.ib(validator=attr.validators.ge(0.))
-    architecture: nn.Module = attr.ib(validator=attr.validators.instance_of(nn.Module))
-    optimizer: torch.optim.Optimizer = attr.ib(validator=attr.validators.instance_of(torch.optim.Optimizer))
-    loss_function: nn.Module = attr.ib(validator=attr.validators.instance_of(nn.Module))
+    architecture: nn.Module = attr.ib(validator=is_nn_module)  # Custom validation to pass instance check (instance_of checks for exact type and not for superclasses)
+    optimizer: torch.optim.Optimizer = attr.ib(validator=is_optimizer)  # Custom validation
+    loss_function: nn.Module = attr.ib(validator=is_nn_module)  # Custom validation
     optimizer_params: Optional[Dict[str, Any]] = attr.ib(default=None)
