308b2162232ac934b20b49e1ac739bb997ab2831
fpgmina <francesco.mina1986@gmail.com>
2025-03-27 14:44:55 -0400

    up

--- utils/model_utils.py
+++ utils/model_utils.py
@@ -2,6 +2,9 @@ import numpy as np
 import torch.utils.data
 from torch.utils.data import Subset, DataLoader
 
+from training.train import train_model
+from training.train_params import TrainingParams
+
 
 def check_forward_pass(model, train_loader, num_classes=200):
     sample_batch, _ = next(iter(train_loader))  # Get one batch
@@ -18,3 +21,19 @@ def get_subset_loader(dataloader, subset_size=1000) -> torch.utils.data.DataLoad
     subset_dataset = Subset(dataloader.dataset, subset_indices)
     subset_loader = DataLoader(subset_dataset, batch_size=32, shuffle=True)
     return subset_loader
+
+
+def train_on_subset(training_params, train_loader, val_loader=None, epochs=2):
+    assert isinstance(training_params, TrainingParams)
+    training_params_subset = TrainingParams(
+        **{
+            **training_params.__dict__,
+            **{
+                "training_name": f"{training_params.training_name}_SUBSET",
+                "epochs": epochs,
+            },
+        }
+    )
+    train_loader_subset = get_subset_loader(train_loader)
+    train_model(training_params_subset, train_loader_subset, val_loader)
+    print("Finished training on subset.")
