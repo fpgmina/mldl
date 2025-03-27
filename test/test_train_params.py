4828d3c1e34965f110bfe9b03ba277174c5368b6
fpgmina <francesco.mina1986@gmail.com>
2025-03-27 12:36:09 -0400

    refactor

---
+++ test/test_train_params.py
@@ -0,0 +1,20 @@
+import pytest
+from torch import nn
+
+from training.train_params import TrainingParams
+
+
+def test_invalid_optimizer_class():
+    model = nn.Linear(10, 5)
+    loss_function = nn.MSELoss()
+
+    with pytest.raises(TypeError):
+        TrainingParams(
+            training_name="test_training",
+            epochs=10,
+            learning_rate=0.001,
+            model=model,
+            loss_function=loss_function,
+            optimizer_class=nn.Linear,  # Invalid optimizer class
+            optimizer_params={},
+        )
