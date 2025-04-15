99673ea3bc0fc32ef09bd6da72f441cae70b4482
fpgmina <francesco.mina1986@gmail.com>
2025-04-15 18:38:11 +0200

    up

--- models/dino_backbone.py
+++ models/dino_backbone.py
@@ -6,10 +6,12 @@ from utils.model_utils import get_device
 
 def get_dino_backbone_model(num_classes: int = 100):
     device = get_device()
-    backbone = timm.create_model("vit_small_patch16_224_dino", pretrained=True)
+    backbone = timm.create_model(
+        "vit_small_patch16_224_dino", pretrained=True
+    )  # TODO check this is the dino model we are supposed to use
 
     for param in backbone.parameters():
         param.requires_grad = False
 
-    backbone.head = nn.Linear(backbone.head.in_features, num_classes)
+    backbone.head = nn.Linear(384, num_classes)
     return backbone.to(device)
