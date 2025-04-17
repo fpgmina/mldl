4e4684745326d54495bcc4c9972939564593273a
fpgmina <francesco.mina1986@gmail.com>
2025-04-17 11:39:07 +0200

    add docstring to numpy_random_seed context manager

--- utils/numpy_utils.py
+++ utils/numpy_utils.py
@@ -1,9 +1,17 @@
+from typing import Optional
+
 import numpy as np
 from contextlib import contextmanager
 
 
 @contextmanager
-def numpy_random_seed(seed=None):
+def numpy_random_seed(seed: Optional[int] = None):
+    """
+    Context manager to set the random state with a given seed (or None to make it non-deterministic).
+    Restores the old state upon exiting.
+    Args:
+        seed: optional(int), the seed to set the random state.
+    """
     # get the current random state
     old_state = np.random.get_state()
 
