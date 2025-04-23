1c0289a5ee154d581f5d840b84c66c5e5e93b6ba
fpgmina <francesco.mina1986@gmail.com>
2025-04-23 21:31:39 +0200

    up test_sparse_sgdm_step comments

--- test/test_ssgd.py
+++ test/test_ssgd.py
@@ -6,7 +6,7 @@ from core.model_editing import create_fisher_mask
 from optim.ssgd import SparseSGDM
 
 
-def test_sparse_sgdm_step_applies_mask(tiny_mlp):
+def test_sparse_sgdm_step(tiny_mlp):
     data = torch.randn(32, 10)
     labels = torch.randint(0, 5, (32,))
     dataloader = DataLoader(TensorDataset(data, labels), batch_size=8)
@@ -20,11 +20,9 @@ def test_sparse_sgdm_step_applies_mask(tiny_mlp):
     # Create binary masks
     mask = create_fisher_mask(fisher_diag, model, keep_ratio=0.2)
 
-    # Wrap named parameters in a dict
     named_params = {name: param for name, param in model.named_parameters()}
 
-    # Optimizer with masking
-    optimizer = SparseSGDM(named_params, grad_mask=mask, lr=0.01)
+    optimizer = SparseSGDM(named_params=named_params, grad_mask=mask, lr=0.01)
 
     model.train()
     for inputs, targets in dataloader:
